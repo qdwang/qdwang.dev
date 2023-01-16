33e4deff16ca294617810c35146a841238719827
qdwang <wangqidong@gmail.com>
2023-01-16 15:32:12 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,123 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <a href="/" class="logo">
+      <h1>qdwang</h1>
+    </a>
+
+    <h2>
+      <div class="center">projects</div>
+    </h2>
+    <div class="p">
+      <div class="center"><b>RawWorks</b><a href="https://rawworks.qdwang.dev/">https://rawworks.qdwang.dev/</a>
+        <br>A
+        powerful and
+        versatile
+        web-based photo editing tool that offers advanced features such as real-time histogram rendering, export in
+        industry-standard XMP or PP3 format, fast performance and security for your raw files, in addition to being a
+        Progressive Web App (PWA) that can work offline.
+      </div>
+    </div>
+
+    <div class="p">
+      <div class="center"><b>web3.storage Rust library and client</b>
+        <a href="https://github.com/qdwang/w3s-rs">w3s-rs</a>
+        <a href="https://github.com/qdwang/w3s-cli">w3s-cli</a>
+        <br>
+        This is a Rust crate that provides easy uploading and downloading of files and directories to web3.storage with
+        optional encryption and compression. It includes a gateway availability checker and supports concurrent uploads
+        of
+        single files and CAR, as well as automatic file splitting for large files. It can also generate CAR from
+        directories
+        and supports decompression and decryption during upload and download. The aim of the project is to make working
+        with web3 storage more
+        efficient and secure.
+      </div>
+    </div>
+
+    <div class="p">
+      <div class="center"><b>QuickRawPicker</b>
+        <a href="https://github.com/qdwang/QuickRawPicker">Github page</a>
+        <a href="https://github.com/qdwang/QuickRawPicker/releases/latest">Download</a>
+        <br>
+        A free and open-source program that allows photographers to efficiently view, rate, and edit
+        raw photos captured by their camera. It is built with C++ and the Godot game engine, making it fast and
+        efficient in processing large numbers of photos. It is compatible with most free raw editing software on the
+        market, and supports multiple platforms including Windows, Linux, and macOS. It has a variety of features such
+        as the ability to compare multiple photos at the same time, read and write XMP and PP3 ratings, adjust EV and
+        shift Gamma, draw highlight and shadow clipping areas, and display AF points for various camera models.
+        Additionally, it is small in size, easy to use and efficient.
+      </div>
+    </div>
+
+    <div class="p">
+      <div class="center"><b>quickraw</b>
+        <a href="https://github.com/qdwang/quickraw">Github page</a>
+        <br>
+        A pure rust library to handle camera raw files.
+      </div>
+    </div>
+
+    <div class="p">
+      <div class="center"><b>quickexif</b>
+        <a href="https://github.com/qdwang/quickraw">Github page</a>
+        <br>
+        A rust library to parse EXIF data from images.
+      </div>
+    </div>
+
+    <div class="p">
+      <div class="center"><b>onshape-level-gen</b>
+        <a href="https://github.com/qdwang/onshape-level-gen">Github page</a>
+        <a href="https://github.com/qdwang/onshape-level-gen/releases/latest">Download</a>
+        <br>
+        An auto level generator for OnShape VR game.
+        This program can generate OnShape levels in 3 difficulties(easy, medium, hard) for each .ogg file.
+      </div>
+    </div>
+
+    <h2>
+      <div class="center">about</div>
+    </h2>
+
+    <div class="p">
+      <div class="center">
+        <p>
+          I have experience in various programming languages such as Rust, C++, and JavaScript and have created several
+          projects related to photo editing, web3 storage, and game level generation. My goal
+          with
+          these projects is to develop efficient and user-friendly tools that can help others with their tasks. I
+          believe
+          that technology should be accessible to everyone and I strive to make my tools easy to use and understand.
+        </p>
+        <p>
+          In my free time, I enjoy learning about new technologies and experimenting with new ideas. I am constantly
+          looking for ways to improve my skills and make a positive impact on others.
+        </p>
+        <p>
+          Thank you for visiting my website. Don't hesitate to reach out to me if you have any questions or suggestions
+          for my projects.
+        </p>
+      </div>
+    </div>
+
+    <h2>
+      <div class="center">contact</div>
+    </h2>
+
+    <div class="p">
+      <div class="center">
+        Email: <a href="hello@qdwang.dev">hello@qdwang.dev</a>
+        <br>
+        Github: <a href="https://github.com/qdwang">https://github.com/qdwang</a>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
